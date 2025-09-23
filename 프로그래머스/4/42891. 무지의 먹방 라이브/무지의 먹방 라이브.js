88c9d9248495801770deffcf00e259aeccf73366
jinhyukSeo <77399457+jinhyukSeo777@users.noreply.github.com>
2025-09-24 05:13:57 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 327.59 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.js
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.js
@@ -1,39 +1,31 @@
 function solution(food_times, k) {
-  const total = food_times.reduce((a, c) => a + c, 0);
-  if (total <= k) return -1;
+    const total = food_times.reduce((acc, cur) => acc + cur, 0);
+    const arr = food_times.map((t, i) => [t, i]);
+    arr.sort((a, b) => a[0] - b[0]);
+    
+    if (total <= k) return -1;
+    if(food_times.length > k) return k+1;
 
-  const foods = food_times.map((t, i) => [t, i]);
-  foods.sort((a, b) => a[0] - b[0]);
+    let prev = 0;
+    
+    for(let i = 0; i < arr.length; i++) {
+        const cur = arr[i][0];
+        const diff = cur - prev;
+        const remain = arr.length - i;
+        const cost = diff * remain;
+        
+        if(cost > k) {
+            const step = Math.floor(k / remain);
+            const rest = k % remain;
+            const min = prev + step;
 
-  let prev = 0;
-  let i = 0;
-  const n = foods.length;
-
-  while (i < n) {
-    const cur = foods[i][0];
-    const diff = cur - prev;
-    if (diff > 0) {
-      const remain = n - i;
-      const cost = diff * remain;
-
-      if (k < cost) {
-        const step = Math.floor(k / remain);
-        const timeLevel = prev + step;
-        const rest = k % remain;
-
-        const alive = [];
-        for (let j = i; j < n; j++) {
-          if (foods[j][0] > timeLevel) alive.push(foods[j][1]);
+            const temp = arr.filter((v)=>v[0] > min);
+            temp.sort((a,b)=>a[1]-b[1]);
+            
+            return temp[rest][1] + 1;
         }
-        alive.sort((a, b) => a - b);
-
-        return alive[rest] + 1;
-      }
-
-      k -= cost;
-      prev = cur;
-
+        
+        k-=cost;
+        prev = cur;
     }
-    i++;
-  }
 }
