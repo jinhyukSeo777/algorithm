899e6e2f017d75bb489c1b7a2f5bc705832b34f6
jinhyukSeo <77399457+jinhyukSeo777@users.noreply.github.com>
2025-09-25 16:38:24 +0900

    [level 2] Title: [1차] 캐시, Time: 295.41 ms, Memory: 41.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -0,0 +1,31 @@
+function solution(cacheSize, cities) {
+    var answer = 0;
+    const map = new Map();
+    
+    if(cacheSize === 0) return cities.length * 5;
+    
+    for(let i = 0; i < cities.length; i++) {
+        const city = cities[i].toUpperCase();
+        
+        if(map.has(city)) {
+            answer+=1;
+            map.set(city, i);
+            continue;
+        }
+     
+        if(map.size < cacheSize) {
+            map.set(city, i);
+            answer+=5;
+            continue;
+        }
+        
+        const arr = [...map].sort((a,b)=>a[1]-b[1]);
+        const target = arr[0][0];
+        
+        map.delete(target);
+        map.set(city, i);
+        answer+=5;
+    }
+    
+    return answer;
+}
