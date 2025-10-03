053b4b6fa24b474fa98b3be61f8869ce4bc7bcc5
jinhyukSeo <77399457+jinhyukSeo777@users.noreply.github.com>
2025-10-03 16:49:56 +0900

    [level 3] Title: [1차] 추석 트래픽, Time: 163.59 ms, Memory: 41 MB -BaekjoonHub

---
+++ 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.js
@@ -0,0 +1,34 @@
+function toSec(str) {
+    const time = str.split(" ")[1];
+    const duration = str.split(" ")[2];
+    
+    const hour = time.split(":")[0];
+    const min = time.split(":")[1];
+    const sec = time.split(":")[2].split(".")[0];
+    const msec = time.split(":")[2].split(".")[1];
+    
+    const endTime = +hour * 60 * 60 * 1000 + +min * 60 * 1000 + +sec * 1000 + +msec;
+    const diff = +duration.split("s")[0] * 1000;
+    const startTime = endTime - diff + 1;
+    
+    return [startTime, endTime];
+}
+
+function solution(lines) {
+    var answer = 0;
+    lines.sort((a,b)=>{
+        const [startTimeA, endTimeA] = toSec(a);
+        const [startTimeB, endTimeB] = toSec(b);
+        return startTimeA-startTimeB;
+    })
+    let arr = [];
+    
+    for(let line of lines) {
+        const [startTime, endTime] = toSec(line);
+        arr = arr.filter((v)=>v+1000 > startTime);
+        arr.push(endTime);
+        answer = Math.max(answer, arr.length);
+    }
+    
+    return answer;
+}
