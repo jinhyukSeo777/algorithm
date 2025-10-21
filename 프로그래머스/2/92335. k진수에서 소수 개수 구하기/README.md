d53b3233bb09d45f96b537978521600b87ae2ac3
jinhyukSeo <77399457+jinhyukSeo777@users.noreply.github.com>
2025-10-22 03:21:16 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 37.49 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] k진수에서 소수 개수 구하기 - 92335 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335) 
 
 ### 성능 요약
 
-메모리: 36.7 MB, 시간: 34.83 ms
+메모리: 36.2 MB, 시간: 37.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 01일 06:55:00
+2025년 10월 22일 03:21:15
 
 ### 문제 설명
 
--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -1,6 +1,5 @@
 function isPrime(num) {
-    if (num === 0 || num === 1) return false;
-    if (num === 2 || num === 3) return true;
+    if (num === 1) return false;
     
     for (let i = 2; i * i <= num; i++) {
         if (num % i === 0) return false;
@@ -11,8 +10,8 @@ function isPrime(num) {
 
 function solution(n, k) {
     const str = n.toString(k);
-    const arr = str.split("0");
-    
+    const arr = str.split("0").filter(Boolean);
+
     return arr.filter((v)=>isPrime(+v)).length;
    
 }
