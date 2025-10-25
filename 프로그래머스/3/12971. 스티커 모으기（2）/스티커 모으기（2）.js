ab87b7589f6120753e4ba96fba2b2cae25f1efbe
jinhyukSeo <77399457+jinhyukSeo777@users.noreply.github.com>
2025-10-25 22:16:09 +0900

    [level 3] Title: 스티커 모으기(2), Time: 9.88 ms, Memory: 41.8 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
@@ -1,6 +1,4 @@
 function solution(sticker) {
-    var answer = 0;
-    
     if(sticker.length <= 2) return Math.max(...sticker);
     
     const dp1 = [];
@@ -8,7 +6,6 @@ function solution(sticker) {
     dp1[1] = sticker[0];
     for(let i = 2; i < sticker.length-1; i++) {
         dp1[i] = Math.max(dp1[i-2]+sticker[i], dp1[i-1]);
-        answer = Math.max(answer, dp1[i]);
     }
     
     const dp2 = [];
@@ -16,8 +13,7 @@ function solution(sticker) {
     dp2[1] = sticker[1];
     for(let i = 2; i < sticker.length; i++) {
         dp2[i] = Math.max(dp2[i-2]+sticker[i], dp2[i-1]);
-        answer = Math.max(answer, dp2[i]);
     }
     
-    return answer;
+    return Math.max(dp1[sticker.length-2], dp2[sticker.length-1]);
 }
