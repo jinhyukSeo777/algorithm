ef3bc3748534808d048ade248cb8811933e349e7
jinhyukSeo <77399457+jinhyukSeo777@users.noreply.github.com>
2025-10-25 22:13:59 +0900

    [level 4] Title: 도둑질, Time: 41.73 ms, Memory: 50.8 MB -BaekjoonHub

---
+++ 프로그래머스/4/42897. 도둑질/도둑질.js
@@ -0,0 +1,21 @@
+function solution(money) {
+    var answer = 0;
+    
+    const dp1 = new Array(money.length);
+    dp1[0] = money[0];
+    dp1[1] = money[0];
+    for(let i = 2; i < money.length-1; i++) {
+        dp1[i] = Math.max(dp1[i-1], dp1[i-2]+money[i]);
+        answer = Math.max(answer, dp1[i]);
+    }
+    
+    const dp2 = new Array(money.length);
+    dp2[0] = 0;
+    dp2[1] = money[1];
+    for(let i = 2; i < money.length; i++) {
+        dp2[i] = Math.max(dp2[i-1], dp2[i-2]+money[i]);
+        answer = Math.max(answer, dp2[i]);
+    }
+    
+    return answer;
+}
