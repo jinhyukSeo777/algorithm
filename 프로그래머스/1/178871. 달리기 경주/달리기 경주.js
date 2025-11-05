80d719344389bce93d4eaa053d26d25f121954b9
jinhyukSeo <77399457+jinhyukSeo777@users.noreply.github.com>
2025-11-06 05:47:43 +0900

    [level 1] Title: 달리기 경주, Time: 217.91 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -0,0 +1,18 @@
+function solution(players, callings) {
+    const map = new Map();
+    
+    players.forEach((v,i)=>map.set(v, i));
+    
+    for(let calling of callings) {
+        const index = map.get(calling);
+        const temp = players[index-1];
+        
+        players[index-1] = players[index];
+        players[index] = temp;
+        
+        map.set(calling, index-1);
+        map.set(temp, index);
+    }
+    
+    return players;
+}
