d084c5cecfd4777f3e1ebb54b4c932777ea11b12
jinhyukSeo <77399457+jinhyukSeo777@users.noreply.github.com>
2025-11-08 19:44:58 +0900

    [level 3] Title: 입국심사, Time: 119.72 ms, Memory: 41.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.js
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -1,13 +1,13 @@
 function solution(n, times) {
+    var answer = 0;
     let min = 1, max = 1000000000000000000;
-    let answer = 0;
     
     while(min <= max) {
         const mid = Math.floor((min+max)/2);
-        const possible = times.reduce((acc,cur)=>acc+Math.floor((mid/cur)),0);
+        const pass = times.reduce((acc,cur)=>acc+Math.floor((mid/cur)), 0);
         
-        if(possible >= n) {
-            answer = mid
+        if(pass >= n) {
+            answer = mid;
             max = mid-1;
         } else {
             min = mid+1;
