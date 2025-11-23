5b56ac2df8b06d7d291fc4b1ae69b80e44ffeb98
jinhyukSeo <77399457+jinhyukSeo777@users.noreply.github.com>
2025-11-24 03:18:09 +0900

    [level 2] Title: 지게차와 크레인, Time: 62.32 ms, Memory: 43.5 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.js
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.js
@@ -1,30 +1,30 @@
 function getStatus(arr) {
-    const column = arr.length;
-    const row = arr[0].length;
+    const height = arr.length;
+    const width = arr[0].length;
     const d = [[1,0], [-1, 0], [0, 1], [0, -1]];
-    const result = new Array(column).fill(null).map((v)=>new Array(row).fill(false));
+    const result = new Array(height).fill(null).map((v)=>new Array(width).fill(false));
     
     const queue = [];
-    for(let i = 0; i < column; i++) {
+    for(let i = 0; i < height; i++) {
         result[i][0] = true;
         queue.push([i, 0]);
         
-        result[i][row-1] = true;
-        queue.push([i, row-1]);
+        result[i][width-1] = true;
+        queue.push([i, width-1]);
     }
-    for(let j = 0; j < row; j++) {
+    for(let j = 0; j < width; j++) {
         result[0][j] = true;
         queue.push([0, j]);
         
-        result[column-1][j] = true;
-        queue.push([column-1, j]);
+        result[height-1][j] = true;
+        queue.push([height-1, j]);
     }
     
     while(queue.length > 0) {
         const [y, x] = queue.shift();
         for(let [dy, dx] of d) {
             const ny = y + dy, nx = x + dx;
-            if(ny >= 0 && ny < column && nx >= 0 && nx < row) {
+            if(ny >= 0 && ny < height && nx >= 0 && nx < width) {
                 if(result[ny][nx] === false && arr[y][x] === false) {
                     result[ny][nx] = true;
                     queue.push([ny, nx]);
@@ -37,29 +37,28 @@ function getStatus(arr) {
 }
 
 function solution(storage, requests) {
-    var answer = 0;
-    const column = storage.length;
-    const row = storage[0].length;
-    const arr = new Array(column).fill(null).map((v)=>new Array(row).fill(true));
+    const height = storage.length;
+    const width = storage[0].length;
+    const arr = new Array(height).fill(null).map((v)=>new Array(width).fill(true));
     
     for(let request of requests) {
-        if(request.length === 1) {
-            const temp = getStatus(arr);
-            for(let i = 0; i < column; i++) {
-                for(let j = 0; j < row; j++) {
-                    if(storage[i][j] === request[0] && temp[i][j] === true) arr[i][j] = false;
+        if(request.length === 2) {
+            for(let i = 0; i < height; i++) {
+                for(let j = 0; j < width; j++) {
+                    if(storage[i][j] === request[0]) arr[i][j] = false;
                 }
             }
         }
         
-        if(request.length === 2) {
-            for(let i = 0; i < column; i++) {
-                for(let j = 0; j < row; j++) {
-                    if(storage[i][j] === request[0]) arr[i][j] = false;
+        if(request.length === 1) {
+            const temp = getStatus(arr);
+            for(let i = 0; i < height; i++) {
+                for(let j = 0; j < width; j++) {
+                    if(storage[i][j] === request[0] && temp[i][j] === true) arr[i][j] = false;
                 }
             }
         }
     }
     
-    return arr.flat().filter((v)=>v).length;
+    return arr.flat().filter(v=>v).length;
 }
