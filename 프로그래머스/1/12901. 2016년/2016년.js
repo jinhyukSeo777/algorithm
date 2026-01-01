c35885fb76161f60ced67a6c070f5b85a4417b67
jinhyukSeo <77399457+jinhyukSeo777@users.noreply.github.com>
2026-01-02 08:14:05 +0900

    [level 1] Title: 2016년, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,12 +1,12 @@
 function solution(a, b) {
-    let n = 4;
+    let n = 0;
     const month = [null, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    const week = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
+    const week = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"];
   
     for(let i = 1; i <= 12; i++) {
         for(let j = 1; j <= month[i]; j++) {
             if(i === a && j === b) return week[n];
-            n = (n+1)%7;
+            n = (n+1) % 7;
         }
     }
 }
