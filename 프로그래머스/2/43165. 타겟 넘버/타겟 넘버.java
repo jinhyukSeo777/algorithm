c81e095485e9cf0db300252b36b6f88f71e42c08
jinhyukSeo <77399457+jinhyukSeo777@users.noreply.github.com>
2026-02-18 23:38:30 +0900

    [level 2] Title: 타겟 넘버, Time: 6.42 ms, Memory: 90.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    int answer = 0;
+    
+    public void dfs(int[] numbers, int target, int sum, int index) {
+        if(index == numbers.length) {
+            if(sum == target) answer++;
+            return;
+        }
+        dfs(numbers, target, sum+numbers[index], index+1);
+        dfs(numbers, target, sum-numbers[index], index+1);
+    }
+    
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0, 0);
+        
+        return answer;
+    }
+}
