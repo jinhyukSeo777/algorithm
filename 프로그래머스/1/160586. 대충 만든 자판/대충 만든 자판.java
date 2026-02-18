20c1cdcea2e2597c0dbbd4f5aa0dc132dccfa59f
jinhyukSeo <77399457+jinhyukSeo777@users.noreply.github.com>
2026-02-18 19:57:46 +0900

    [level 1] Title: 대충 만든 자판, Time: 2.57 ms, Memory: 90.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] keymap, String[] targets) {
+        int[] answer = new int[targets.length];
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < keymap.length; i++) {
+            String str = keymap[i];
+            
+            for(int j = 0; j < str.length(); j++) {
+                String ch = str.substring(j, j+1);
+                
+                if(map.containsKey(ch)) {
+                    map.put(ch, Math.min(j+1, map.get(ch)));
+                } else {
+                    map.put(ch, j+1);
+                }
+            }
+        }
+        
+        for(int i = 0; i < targets.length; i++) {
+            String str = targets[i];
+            int sum = 0;
+            
+            for(int j = 0; j < str.length(); j++) {
+                String ch = str.substring(j, j+1);
+                
+                if(!map.containsKey(ch)) {
+                    sum = -1;
+                    break;
+                }
+                
+                sum+=map.get(ch);
+            }
+            
+            answer[i] = sum;
+        }
+        
+        return answer;
+    }
+}
