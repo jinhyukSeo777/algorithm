804e156958422a99e4fb97552f14b8795b15f54d
jinhyukSeo <77399457+jinhyukSeo777@users.noreply.github.com>
2026-03-03 01:40:46 +0900

    [level 3] Title: 합승 택시 요금, Time: 56.00 ms, Memory: 45.2 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.js
@@ -1,41 +1,73 @@
-function solution(n, s, a, b, fares) {
-    const map = new Map();
+// function solution(n, s, a, b, fares) {
+//     const map = new Map();
     
-    for(let [a, b, cost] of fares) {
-        if(!map.has(a)) map.set(a, []);
-        if(!map.has(b)) map.set(b, []);
-        map.get(a).push([b, cost]);
-        map.get(b).push([a, cost]);
+//     for(let [a, b, cost] of fares) {
+//         if(!map.has(a)) map.set(a, []);
+//         if(!map.has(b)) map.set(b, []);
+//         map.get(a).push([b, cost]);
+//         map.get(b).push([a, cost]);
+//     }
+    
+//     function dijkstra(start) {
+//         const arr = new Array(n+1).fill(Infinity);
+        
+//         const queue = [];
+//         queue.push(start);
+//         arr[start] = 0;
+        
+//         while(queue.length > 0) {
+//             const current = queue.shift();
+//             for(let [next, cost] of map.get(current) || []) {
+//                 if(arr[current]+cost < arr[next]) {
+//                     queue.push(next);
+//                     arr[next] = arr[current]+cost;
+//                 }
+//             }
+//         }
+        
+//         return arr;
+//     }
+    
+//     const dijkA = dijkstra(a);
+//     const dijkB = dijkstra(b);
+//     const dijkS = dijkstra(s);
+    
+//     let min = Infinity;
+//     for(let i = 1; i <= n; i++) {
+//         min = Math.min(min, dijkA[i]+dijkB[i]+dijkS[i]);
+//     }
+    
+//     return min;
+// }
+
+function solution(n, s, a, b, fares) {
+    var answer = Infinity;
+    const arr = new Array(n+1).fill(null).map(v => new Array(n+1).fill(Infinity));
+
+    for(let i = 1; i <= n; i++) {
+        arr[i][i] = 0;
+    }
+
+    for(let [from, to, cost] of fares) {
+        arr[from][to] = cost;
+        arr[to][from] = cost;
     }
     
-    function dijkstra(start) {
-        const arr = new Array(n+1).fill(Infinity);
-        
-        const queue = [];
-        queue.push(start);
-        arr[start] = 0;
-        
-        while(queue.length > 0) {
-            const current = queue.shift();
-            for(let [next, cost] of map.get(current) || []) {
-                if(arr[current]+cost < arr[next]) {
-                    queue.push(next);
-                    arr[next] = arr[current]+cost;
+    for(let k = 1; k <= n; k++) {
+        for(let i = 1; i <= n; i++) {
+            if(arr[i][k] !== Infinity) {
+                for(let j = 1; j <= n; j++) {
+                    if(arr[i][j] > arr[i][k] + arr[k][j]) {
+                        arr[i][j] = arr[i][k] + arr[k][j];
+                    }
                 }
             }
         }
-        
-        return arr;
     }
-    
-    const dijkA = dijkstra(a);
-    const dijkB = dijkstra(b);
-    const dijkS = dijkstra(s);
-    
-    let min = Infinity;
+
     for(let i = 1; i <= n; i++) {
-        min = Math.min(min, dijkA[i]+dijkB[i]+dijkS[i]);
+        answer = Math.min(answer, arr[s][i] + arr[i][a] + arr[i][b]);
     }
-    
-    return min;
+
+    return answer;
 }
