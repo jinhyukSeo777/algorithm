71d5f30c470111c47686aa71ef8bceaa8c60bc1e
jinhyukSeo <77399457+jinhyukSeo777@users.noreply.github.com>
2026-03-14 16:33:30 +0900

    [level 3] Title: 야근 지수, Time: 29.60 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/README.md
+++ 프로그래머스/3/12927. 야근 지수/README.md
@@ -1,10 +1,10 @@
 # [level 3] 야근 지수 - 12927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12927?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12927) 
 
 ### 성능 요약
 
-메모리: 36.1 MB, 시간: 27.44 ms
+메모리: 36.2 MB, 시간: 29.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 22일 05:40:14
+2026년 03월 14일 16:33:27
 
 ### 문제 설명
 
--- 프로그래머스/3/12927. 야근 지수/야근 지수.js
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -1,21 +1,18 @@
 function solution(n, works) {
     works.sort((a,b)=>a-b);
     works.unshift(0);
-    let index = works.length-2;
+    let target = works.length-2;
     
-    while(n > 0 && index >= 0) {
-        const target = works[index];
-        
-        while(n > 0 && works[index+1] > works[index]) {
-            for(let i = index+1; i < works.length; i++) {
+    while(target >= 0 && n > 0) {
+        while(works.at(-1) !== works[target] && n > 0) {
+            for(let i = target+1; i < works.length; i++) {
                 if(n === 0) break;
                 works[i]--;
                 n--;
             }
         }
-        
-        index--;
+        target--;
     }
     
-    return works.reduce((acc,cur)=>acc+cur*cur, 0);
+    return works.reduce((acc,cur)=>acc+Math.pow(cur, 2), 0);
 }
