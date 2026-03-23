c86bcb0a12a3b2b85c871302a198ec70da75f221
jinhyukSeo <77399457+jinhyukSeo777@users.noreply.github.com>
2026-03-23 16:34:35 +0900

    [level 4] Title: 도둑질, Time: 28.85 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.js
+++ 프로그래머스/4/42897. 도둑질/도둑질.js
@@ -1,17 +1,19 @@
 function solution(money) {
-    const dp1 = new Array(money.length);
+    const len = money.length;
+    
+    const dp1 = new Array(len);
     dp1[0] = money[0];
     dp1[1] = money[0];
-    for(let i = 2; i < money.length-1; i++) {
-        dp1[i] = Math.max(dp1[i-1], dp1[i-2]+money[i]);
+    for(let n = 2; n < len-1; n++) {
+        dp1[n] = Math.max(dp1[n-1], dp1[n-2] + money[n]);
     }
     
-    const dp2 = new Array(money.length);
+    const dp2 = new Array(len);
     dp2[0] = 0;
     dp2[1] = money[1];
-    for(let i = 2; i < money.length; i++) {
-        dp2[i] = Math.max(dp2[i-1], dp2[i-2]+money[i]);
+    for(let n = 2; n < len; n++) {
+        dp2[n] = Math.max(dp2[n-1], dp2[n-2] + money[n]);
     }
     
-    return Math.max(dp1[money.length-2], dp2[money.length-1]);
+    return Math.max(dp1[len-2], dp2[len-1]);
 }
