e2e6a2de15b4b199753b559f00465906e92bee42
jinhyukSeo <77399457+jinhyukSeo777@users.noreply.github.com>
2026-04-03 16:15:34 +0900

    [level 3] Title: 징검다리 건너기, Time: 49.10 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.js
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.js
@@ -1,24 +1,35 @@
+function canSuccess(mid, stones, k) {
+    let count = 0;
+    
+    for(let stone of stones) {
+        if(stone >= mid) {
+            count = 0;
+        } else {
+            count++;
+        }
+        
+        if(count === k) return false; 
+    }
+    
+    return true;
+}
+
 function solution(stones, k) {
     var answer = 0;
     let min = 1, max = 200000000;
     
-    while(min < max) {
+    while(min <= max) {
         const mid = Math.floor((min+max)/2);
         
-        let count = 0;
-        let success = true;
-        for(let stone of stones) {
-            if(stone >= mid) count = 0;
-            else count++;
-            if(count === k) success = false; 
-        }
+        const success = canSuccess(mid, stones, k);
         
         if(success) {
+            answer = mid;
             min = mid+1;
         } else {
-            max = mid;
+            max = mid-1;
         }
     }
     
-    return min-1;
+    return answer;
 }
