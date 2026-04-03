7d6ed67d9f4cbc00840c7e1d5c6f89e6e4910f6c
jinhyukSeo <77399457+jinhyukSeo777@users.noreply.github.com>
2026-04-03 18:16:02 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 39.47 ms, Memory: 40.6 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,22 +1,24 @@
-function splitStringByNumber(input) {
-    // const [_, prefix, number, suffix] = input.match(/^([^0-9]+)([0-9]+)(.*)$/);
+function go(input) {
     const [prefix, suffix] = input.split(/[0-9]+/);
     const number = input.match(/[0-9]+/);
     return [prefix.toLowerCase(), Number(number), suffix];
 }
 
 function solution(files) {
-    // const str = "img12.png img34.png img56.png";
-    // console.log(str.match(/^([^0-9]+)([0-9]+)(.*)$/));
-    // console.log(str.match(/[^0-9]+[0-9]+.*/g));
-    // console.log(/[^0-9]+[0-9]+.*/.test(str))   //
-    // console.log(/^[^0-9]+[0-9]+.*$/.test(str)) // 둘이 다름
-    // console.log(str.match(/[a-zA-Z]+[0-9]+\.png/g));
-    // console.log(str.split(/[a-zA-Z]+[0-9]+\.png/));
+//     const str = "img12.png img34.png img56.png";
+//     console.log(str.match(/([^0-9]+)([0-9]+)(.*)/));
+    
+//     console.log(/[^0-9]+[0-9]+.*/.test(str))
+//     console.log(/^[^0-9]+[0-9]+.*$/.test(str))
+    
+//     console.log(str.match(/[a-zA-Z]+[0-9]+\.png/));
+//     console.log(str.match(/[a-zA-Z]+[0-9]+\.png/g));
+    
+//     console.log(str.split(/[a-zA-Z]+[0-9]+\.png/));
     
     return files.sort((a,b)=>{
-        const result1 = splitStringByNumber(a);
-        const result2 = splitStringByNumber(b);
+        const result1 = go(a);
+        const result2 = go(b);
         if(result1[0] !== result2[0]) return result1[0].localeCompare(result2[0]);
         if(result1[1] !== result2[1]) return result1[1] - result2[1];
     })
