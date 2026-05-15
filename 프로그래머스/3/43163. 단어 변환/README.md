8e12ce9615082261522d2d0b1445b9f8eb54f633
jinhyukSeo <77399457+jinhyukSeo777@users.noreply.github.com>
2026-05-16 08:08:42 +0900

    [level 3] Title: 단어 변환, Time: 0.29 ms, Memory: 32.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=javascript) 
 
 ### 성능 요약
 
-메모리: 74.3 MB, 시간: 0.21 ms
+메모리: 32.5 MB, 시간: 0.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 19일 04:18:45
+2026년 05월 16일 08:08:38
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,26 +1,29 @@
 function isNear(str, target) {
-    const diff_count = str.split("").reduce((acc,_,idx)=>{
-        if(str[idx] !== target[idx]) return acc+1;
-        else return acc;
-    },0);
-    return diff_count === 1 ? true : false;
+    let count = 0;
+    
+    for(let i = 0; i < str.length; i++) {
+        if(str[i] !== target[i]) count++;
+    }
+    
+    return count < 2 ? true : false;
 }
 
 function solution(begin, target, words) {
     var answer = Infinity;
-    let check  = new Array(words.length).fill(false);
+    const set = new Set();
     
     function dfs(str, count) {
         if(str === target) {
             answer = Math.min(answer, count);
             return;
         }
-        for(let i = 0; i < words.length; i++) {
-            if(!check[i] && isNear(str, words[i])) {
-                check[i] = true;
-                dfs(words[i], count+1);
-                check[i] = false;
-            }
+        
+        for(let word of words) {
+            if(set.has(word) || !isNear(str, word)) continue;
+            
+            set.add(word);
+            dfs(word, count + 1);
+            set.delete(word);
         }
     }
     
